0066c8dca8fca7283c491a6cd36456df3289fc11
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2023-08-21 01:19:19 +0900

    Replace plan with price in subscription_items_examples

--- spec/shared_stripe_examples/subscription_items_examples.rb
+++ spec/shared_stripe_examples/subscription_items_examples.rb
@@ -3,42 +3,42 @@ require 'spec_helper'
 shared_examples 'Subscription Items API' do
   let(:stripe_helper) { StripeMock.create_test_helper }
   let(:product) { stripe_helper.create_product(name: 'Silver Product') }
-  let(:plan) { stripe_helper.create_plan(product: product.id, id: 'silver_plan') }
-  let(:plan2) { stripe_helper.create_plan(amount: 100, id: 'one_more_1_plan', product: product.id) }
+  let(:price) { stripe_helper.create_plan(product: product.id, id: 'silver_price') }
+  let(:price2) { stripe_helper.create_plan(amount: 100, id: 'one_more_1_price', product: product.id) }
   let(:customer) { Stripe::Customer.create(source: stripe_helper.generate_card_token) }
-  let(:subscription) { Stripe::Subscription.create(customer: customer.id, items: [{ plan: plan.id }]) }
+  let(:subscription) { Stripe::Subscription.create(customer: customer.id, items: [{ price: price.id }]) }
 
   context 'creates an item' do
     it 'when required params only' do
-      item = Stripe::SubscriptionItem.create(plan: plan.id, subscription: subscription.id)
+      item = Stripe::SubscriptionItem.create(price: price.id, subscription: subscription.id)
 
       expect(item.id).to match(/^test_si/)
-      expect(item.plan.id).to eq(plan.id)
+      expect(item.price.id).to eq(price.id)
       expect(item.subscription).to eq(subscription.id)
     end
     it 'when no subscription params' do
-      expect { Stripe::SubscriptionItem.create(plan: plan.id) }.to raise_error { |e|
+      expect { Stripe::SubscriptionItem.create(price: price.id) }.to raise_error { |e|
         expect(e).to be_a(Stripe::InvalidRequestError)
         expect(e.param).to eq('subscription')
         expect(e.message).to eq('Missing required param: subscription.')
       }
     end
-    it 'when no plan params' do
+    it 'when no price params' do
       expect { Stripe::SubscriptionItem.create(subscription: subscription.id) }.to raise_error { |e|
         expect(e).to be_a(Stripe::InvalidRequestError)
-        expect(e.param).to eq('plan')
-        expect(e.message).to eq('Missing required param: plan.')
+        expect(e.param).to eq('price')
+        expect(e.message).to eq('Missing required param: price.')
       }
     end
   end
 
   context 'updates an item' do
-    let(:item) { Stripe::SubscriptionItem.create(plan: plan.id, subscription: subscription.id, quantity: 2 ) }
+    let(:item) { Stripe::SubscriptionItem.create(price: price.id, subscription: subscription.id, quantity: 2 ) }
 
-    it 'updates plan' do
-      updated_item = Stripe::SubscriptionItem.update(item.id, plan: plan2.id)
+    it 'updates price' do
+      updated_item = Stripe::SubscriptionItem.update(item.id, price: price2.id)
 
-      expect(updated_item.plan.id).to eq(plan2.id)
+      expect(updated_item.price.id).to eq(price2.id)
     end
     it 'updates quantity' do
       updated_item = Stripe::SubscriptionItem.update(item.id, quantity: 23)
@@ -56,8 +56,8 @@ shared_examples 'Subscription Items API' do
 
   context 'retrieves a list of items' do
     before do
-      Stripe::SubscriptionItem.create(plan: plan.id, subscription: subscription.id, quantity: 2 )
-      Stripe::SubscriptionItem.create(plan: plan2.id, subscription: subscription.id, quantity: 20)
+      Stripe::SubscriptionItem.create(price: price.id, subscription: subscription.id, quantity: 2 )
+      Stripe::SubscriptionItem.create(price: price2.id, subscription: subscription.id, quantity: 20)
     end
 
     it 'retrieves all subscription items' do
